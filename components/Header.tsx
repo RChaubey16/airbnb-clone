c12beda19e2010b28ed3027c65c2b9e78b7fcba8
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2022-11-19 10:16:45 +0530

    Completed the header component.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import {
+  MagnifyingGlassIcon,
+  GlobeAltIcon,
+  Bars3Icon,
+  UserCircleIcon,
+} from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
@@ -16,6 +21,7 @@ const Header = () => {
           objectPosition="left"
         />
       </div>
+
       {/* middle section */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
@@ -25,8 +31,16 @@ const Header = () => {
         />
         <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 md:mx-2 cursor-pointer " />
       </div>
+
       {/* right section */}
-      <div></div>
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
+          <Bars3Icon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 };
