8c11ceb5ae424c7b40ddf2949fef87f082020c59
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2022-11-19 21:39:23 +0530

    Added the small card and medium card components.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  rejectUnauthorized: false,
   images: {
     domains: [`links.papareact.com`],
   },
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import MediumCard from "../components/MediumCard";
+import SmallCard from "../components/SmallCard";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ exploreData, cardsData }: any) => {
   return (
     <div className="">
       <Head>
@@ -15,8 +17,54 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+
+        {/* Pull some data from a server - API endpoints */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {exploreData?.map((item: any) => (
+            <SmallCard
+              key={item.img}
+              img={item.img}
+              location={item.location}
+              distance={item.distance}
+            />
+          ))}
+        </div>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex p-3 -ml-3 space-x-3 overflow-scroll scrollbar-hide">
+            {cardsData?.map((item: any) => (
+              <MediumCard key={item.img} img={item.img} title={item.title} />
+            ))}
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
+
+// This function tells the server that this page is static and do the execution accordingly.
+export async function getStaticProps() {
+  const exploreData = await fetch(`https://www.jsonkeeper.com/b/4G1G`).then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch(`https://www.jsonkeeper.com/b/VHHT`).then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
+}
 
 export default Home;
