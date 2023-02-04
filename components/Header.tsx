05046a0e29803a3610cb613660c961f3e27d54f6
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2023-02-04 19:07:30 +0530

    Added the calendar feature.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import {
   MagnifyingGlassIcon,
   GlobeAltIcon,
   Bars3Icon,
   UserCircleIcon,
+  UsersIcon,
 } from "@heroicons/react/24/solid";
+// React Date range default css imports
+import { DateRangePicker, RangeKeyDict } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 
 const Header = () => {
+  const [searchInput, setSearchInput] = useState(``);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberOfGuests, setNumberOfGuests] = useState<number>(1);
+  // Initial date range for the date library.
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: `selection`,
+  };
+
+  const handleSelect = (ranges: RangeKeyDict): void => {
+    // ranges data comes from the react date range library itself.
+    console.log(`RANGES`, ranges);
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left section */}
@@ -25,6 +48,8 @@ const Header = () => {
       {/* middle section */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Start your search"
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
@@ -41,6 +66,37 @@ const Header = () => {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+
+      {/* Calendar */}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-8">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()} // Minimum or start date should be today.
+            rangeColors={[`#FD5B61`]}
+            onChange={(ranges: RangeKeyDict): void => handleSelect(ranges)}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={numberOfGuests}
+              onChange={(e) => setNumberOfGuests(Number(e.target.value))}
+              min={1}
+              type="number"
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button className="flex-grow" onClick={() => setSearchInput(``)}>
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
